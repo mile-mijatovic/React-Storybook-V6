2698fdb86275f3089858d85ddb0ac55d0753fc4e
Mile Mijatovic <mile@milemijatovic.com>
2021-12-22 21:29:21 +0100

    Storybook update

---
+++ .storybook/preview.js
@@ -0,0 +1,22 @@
+export const parameters = {
+  actions: { argTypesRegex: "^on[A-Z].*" },
+  controls: {
+    matchers: {
+      color: /(background|color)$/i,
+      date: /Date$/,
+    },
+  },
+  options: {
+    /**
+     * Sorting Stories
+     * By default, stories are sorted in the order in which they were imported
+     * @param {*} a
+     * @param {*} b
+     * @returns
+     */
+    storySort: (a, b) =>
+      a[1].kind === b[1].kind
+        ? 0
+        : a[1].id.localeCompare(b[1].id, undefined, { numeric: true }),
+  },
+};
